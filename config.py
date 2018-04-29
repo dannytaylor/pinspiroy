3067b8cd41de3cd818b7df0b44116333214eefca
Unknown <dhtxhg@gmail.com>
2018-04-28 18:58:37 -0700

    local config settings

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #user tablet settings
-LEFT_HANDED      = False 	#rotate pen and track 180 if true
+LEFT_HANDED      = True 	#rotate pen and track 180 if true
 TRACKPAD_ENABLED = False #since it's sort of in the way
 
 # playing with these pressure curves types
@@ -10,12 +10,12 @@ PRESSURE_CURVE   = False # LINEAR, SOFT, HARD
 FULL_PRESSURE	 = 1.0		# force needed for full pressure, eg 0.8 pen pressure will give a full stroke
 
 # dimensions of monitor to draw to
-MONITOR_W = 1920
-MONITOR_H = 1080
+MONITOR_W = 2560
+MONITOR_H = 1440
 
 # offset of monitor to draw to from top left
 # e.g. a dual monitor setup where you're drawing on the right-hand monitor X would be the width of the left monitor 
 # for a single monitor setup these are both 0
-MONITOR_X = 0
-MONITOR_Y = 0
+MONITOR_X = 1080
+MONITOR_Y = 256
 
